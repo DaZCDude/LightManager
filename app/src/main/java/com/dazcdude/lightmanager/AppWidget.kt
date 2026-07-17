d36db242647b91c47e10a4678761b5d4817087aa
DaZCDude <84847028+DaZCDude@users.noreply.github.com>
2026-07-17 23:06:28 +0200

    Made widget dynamic and prettier

--- app/src/main/java/com/dazcdude/lightmanager/AppWidget.kt
+++ app/src/main/java/com/dazcdude/lightmanager/AppWidget.kt
@@ -3,12 +3,14 @@ package com.dazcdude.lightmanager
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.util.Log
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.glance.Button
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
 import androidx.glance.action.ActionParameters
 import androidx.glance.action.actionParametersOf
 import androidx.glance.action.actionStartActivity
@@ -17,12 +19,18 @@ import androidx.glance.appwidget.action.ActionCallback
 import androidx.glance.appwidget.action.actionRunCallback
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
+import androidx.glance.color.ColorProvider
 import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
 import androidx.glance.layout.Column
 import androidx.glance.layout.Row
+import androidx.glance.layout.Spacer
 import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.padding
 import androidx.glance.text.Text
+import androidx.glance.text.TextAlign
+import androidx.glance.text.TextStyle
 import org.json.JSONObject
 import java.net.DatagramPacket
 import java.net.DatagramSocket
@@ -61,16 +69,44 @@ class AppWidget: GlanceAppWidget() {
     @Composable
     private fun MyContent(lightObject: LightObject) {
         Column(
-            modifier = GlanceModifier.fillMaxSize().background(Color.White),
+            modifier = GlanceModifier
+                .padding(4.dp)
+                .fillMaxSize()
+                .background(GlanceTheme.colors.surface),
             verticalAlignment = Alignment.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             if (lightObject.ip.isEmpty()) {
-
+                Box(modifier = GlanceModifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center) {
+                    Text(
+                        text = "Configure widget to use",
+                        style = TextStyle(
+                            color = ColorProvider(Color.Black, Color.White),
+                            textAlign = TextAlign.Center
+                        )
+                    )
+                }
             }
             else {
-                Text(text = lightObject.ip, modifier = GlanceModifier.padding(12.dp))
-                Row(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    text = lightObject.displayName,
+                    style = TextStyle(
+                        color = ColorProvider(Color.Black, Color.White)
+                    ),
+                    modifier = GlanceModifier.padding(top = 8.dp)
+                )
+                Text(
+                    text = lightObject.ip,
+                    style = TextStyle(
+                        color = ColorProvider(Color.Black, Color.White)
+                    )
+                )
+
+                Spacer(modifier = GlanceModifier.defaultWeight())
+
+                Row(horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = GlanceModifier.padding(8.dp)) {
                     Button(
                         text = "On",
                         onClick = actionRunCallback<TurnLightOnAction>(
@@ -79,6 +115,8 @@ class AppWidget: GlanceAppWidget() {
                             )
                         )
                     )
+
+                    Spacer(modifier = GlanceModifier.padding(4.dp))
 
                     Button(
                         text = "Off",
