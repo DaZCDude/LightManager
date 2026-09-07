225756b08e2e2b6d588d834d8252c443893f2bd2
DaZCDude <84847028+DaZCDude@users.noreply.github.com>
2026-09-07 17:36:13 +0200

    Added statusbar padding to widget config composable

--- app/src/main/java/com/dazcdude/lightmanager/AppWidgetConfigurationActivity.kt
+++ app/src/main/java/com/dazcdude/lightmanager/AppWidgetConfigurationActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.selection.selectableGroup
@@ -104,7 +105,8 @@ class AppWidgetConfigurationActivity : ComponentActivity()
                 Surface(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(16.dp),
+                        .padding(16.dp)
+                        .statusBarsPadding(),
                     shape = RoundedCornerShape(16.dp),
                     color = MaterialTheme.colorScheme.surface
                 ) {
